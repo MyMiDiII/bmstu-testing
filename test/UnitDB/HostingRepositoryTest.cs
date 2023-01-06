3db65c09e10d2ca2cfb8494a09652ba298de0a22
Ivan Tsvetkov <amunra23@MacBook-Pro-Ivan.local>
2023-01-06 03:49:53 +0300

    lab_01: Добавлено юнит тестирование для репоизтория Hosting

---
+++ test/UnitDB/HostingRepositoryTest.cs
@@ -0,0 +1,141 @@
+using Microsoft.EntityFrameworkCore;
+
+using ServerING.Models;
+using ServerING.Repository;
+
+
+namespace UnitDB {
+    public class HostingRepositoryTest {
+        private void LoadFixtures(TestData data, AppDBContent context) {
+            context.WebHosting.AddRange(data.hosting);
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public void TestHostingAdd() {
+            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+            var options = new DbContextOptionsBuilder<AppDBContent>()
+                .UseInMemoryDatabase(databaseName: myDatabaseName)
+                .Options;
+            var data = new TestData();
+
+            using (var context = new AppDBContent(options)) {
+                HostingRepository hostingRepository = new HostingRepository(context);
+                var actualHosting = hostingRepository.Add(data.hosting);
+
+                Assert.Equal(data.hosting.Name, actualHosting.Name);
+                Assert.Equal(data.hosting.SubMonths, actualHosting.SubMonths);
+                Assert.Equal(data.hosting.PricePerMonth, actualHosting.PricePerMonth);
+
+                var isHostingWithSuchName = context.WebHosting.Where(s => s.Name == data.hosting.Name).Any();
+                Assert.True(isHostingWithSuchName);
+            }
+        }
+
+        [Fact]
+        public void TestHostingUpdate() {
+            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+            var options = new DbContextOptionsBuilder<AppDBContent>()
+                .UseInMemoryDatabase(databaseName: myDatabaseName)
+                .Options;
+            var data = new TestData();
+
+            using (var context = new AppDBContent(options)) {
+                LoadFixtures(data, context);
+                var newName = "test";
+
+                WebHosting updatedHosting = new WebHosting {
+                    Id = 1,
+                    Name = newName,
+                    SubMonths = data.hosting.SubMonths,
+                    PricePerMonth = data.hosting.PricePerMonth,
+                };  
+
+                HostingRepository hostingRepository = new HostingRepository(context);
+                var actualHosting = hostingRepository.Update(updatedHosting);
+
+                Assert.Equal(newName, updatedHosting.Name);
+            }
+        }
+
+        [Fact]
+        public void TestHostingDelete() {
+            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+            var options = new DbContextOptionsBuilder<AppDBContent>()
+                .UseInMemoryDatabase(databaseName: myDatabaseName)
+                .Options;
+            var data = new TestData();
+
+            using (var context = new AppDBContent(options)) {
+                LoadFixtures(data, context);
+                
+                HostingRepository hostingRepository = new HostingRepository(context);
+                var actualHosting = hostingRepository.Delete(id: 1);
+
+                Assert.Equal(data.hosting.Name, actualHosting.Name);
+                Assert.Equal(data.hosting.SubMonths, actualHosting.SubMonths);
+                Assert.Equal(data.hosting.PricePerMonth, actualHosting.PricePerMonth);
+
+                var isHostingWithSuchId = context.WebHosting.Where(s => s.Id == 1).Any();
+                Assert.False(isHostingWithSuchId);
+            }
+        }
+
+        [Fact]
+        public void TestHostingGetById() {
+            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+            var options = new DbContextOptionsBuilder<AppDBContent>()
+                .UseInMemoryDatabase(databaseName: myDatabaseName)
+                .Options;
+            var data = new TestData();
+
+            using (var context = new AppDBContent(options)) {
+                LoadFixtures(data, context);
+
+                HostingRepository hostingRepository = new HostingRepository(context);
+                var actualHosting = hostingRepository.GetByID(1);
+
+                Assert.Equal(data.hosting.Name, actualHosting.Name);
+                Assert.Equal(data.hosting.SubMonths, actualHosting.SubMonths);
+                Assert.Equal(data.hosting.PricePerMonth, actualHosting.PricePerMonth);
+            }
+        }
+
+        [Fact]
+        public void TestHostingGetAll() {
+            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+            var options = new DbContextOptionsBuilder<AppDBContent>()
+                .UseInMemoryDatabase(databaseName: myDatabaseName)
+                .Options;
+            var data = new TestData();
+
+            using (var context = new AppDBContent(options)) {
+                LoadFixtures(data, context);
+
+                HostingRepository hostingRepository = new HostingRepository(context);
+                var actualHostings = hostingRepository.GetAll();
+                Assert.Single(actualHostings);
+            }
+        }
+
+        [Fact]
+        public void TestHostingGetByName() {
+            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+            var options = new DbContextOptionsBuilder<AppDBContent>()
+                .UseInMemoryDatabase(databaseName: myDatabaseName)
+                .Options;
+            var data = new TestData();
+
+            using (var context = new AppDBContent(options)) {
+                LoadFixtures(data, context);
+
+                HostingRepository hostingRepository = new HostingRepository(context);
+                var actualHosting = hostingRepository.GetByName(data.hosting.Name);
+
+                Assert.Equal(data.hosting.Name, actualHosting.Name);
+                Assert.Equal(data.hosting.SubMonths, actualHosting.SubMonths);
+                Assert.Equal(data.hosting.PricePerMonth, actualHosting.PricePerMonth);
+            }
+        }
+    }
+}
