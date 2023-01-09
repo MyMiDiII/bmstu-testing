cddf2ade48603808149de57568cecbc60ca0ea6d
MyMiDiII <mymidi.student@yandex.ru>
2023-01-09 13:39:51 +0300

    lab02: Глава 5. Интеграция. Чисто

--- test/Integrate/test.sh
+++ test/Integrate/test.sh
@@ -1,8 +1,4 @@
-#cat ../sql/init-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
-#cat ../sql/fill-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
 export PGPASSWORD=postgres
-echo "pwd"
-pwd
 psql -h localhost -U postgres -d testdb -f ../sql/init-db.sql
 psql -h localhost -U postgres -d testdb -f ../sql/fill-db.sql
 
@@ -11,7 +7,5 @@ test_exit_code=$?
 
 psql -h localhost -U postgres -d testdb -f ../sql/clear-db.sql
 psql -h localhost -U postgres -d testdb -f ../sql/drop-db.sql
-#cat ../sql/clear-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
-#cat ../sql/drop-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
 
 exit "$test_exit_code"
