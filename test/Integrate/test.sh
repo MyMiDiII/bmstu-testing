8508f660952b2191956aafd082b6932aae5ca2be
MyMiDiII <mymidi.student@yandex.ru>
2023-01-09 12:26:45 +0300

    lab02: Глава 5. Интеграция

---
+++ test/Integrate/test.sh
@@ -0,0 +1,12 @@
+#cat ../sql/init-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
+#cat ../sql/fill-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
+export PGPASSWORD=postgres
+psql -h localhost -U postgres -d testdb -f ../sql/init-db.sql
+psql -h localhost -U postgres -d testdb -f ../sql/fill-db.sql
+
+dotnet test --logger "console;verbosity=detailed"
+
+psql -h localhost -U postgres -d testdb -f ../sql/clear-db.sql
+psql -h localhost -U postgres -d testdb -f ../sql/drop-db.sql
+#cat ../sql/clear-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
+#cat ../sql/drop-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
