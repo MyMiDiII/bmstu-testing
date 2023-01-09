c6b0078cec904075f37ac915ddda8d30280aede2
MyMiDiII <mymidi.student@yandex.ru>
2023-01-09 12:31:25 +0300

    lab02: Глава 5. Интеграция. Текущая директория?

--- test/Integrate/test.sh
+++ test/Integrate/test.sh
@@ -1,6 +1,8 @@
 #cat ../sql/init-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
 #cat ../sql/fill-db.sql | docker exec -i test-testdb-1 psql -U postgres -d testdb
 export PGPASSWORD=postgres
+echo "pwd"
+pwd
 psql -h localhost -U postgres -d testdb -f ../sql/init-db.sql
 psql -h localhost -U postgres -d testdb -f ../sql/fill-db.sql
 
